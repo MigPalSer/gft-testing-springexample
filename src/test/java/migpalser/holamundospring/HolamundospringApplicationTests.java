7fe81e84f222f2b193868c20fca64c9811251446
MigPalSer <mlpv@gft.com>
2022-01-24 12:03:53 +0100

    commit inicial

---
+++ src/test/java/migpalser/holamundospring/HolamundospringApplicationTests.java
@@ -0,0 +1,13 @@
+package migpalser.holamundospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolamundospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
