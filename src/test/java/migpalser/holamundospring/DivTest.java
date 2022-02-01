f5dae56c19dcffc3ec9ddeb05d9eb317f5d76f38
MigPalSer <mlpv@gft.com>
2022-02-01 10:12:56 +0100

    test division añadidos

---
+++ src/test/java/migpalser/holamundospring/DivTest.java
@@ -0,0 +1,28 @@
+package migpalser.holamundospring;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+class DivTest {
+
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/divtest.csv")
+    void csvDivTest(String a, String b, String result, @Autowired TestRestTemplate restTemplate) {
+    	String url="/div?x="+a+"&y="+b;
+    	 String s=restTemplate.getForObject(url, String.class);
+         assertThat(s)
+ 		.isEqualTo(result);
+    }
+}
